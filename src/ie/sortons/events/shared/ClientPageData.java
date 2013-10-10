353aae008c85ae6871e414d18dc048f52b0a5f49
Brian Henry <brian.henry2@gmail.com>
2013-10-10 16:13:02 +0100

    More work on adding pages

---
+++ src/ie/sortons/events/shared/ClientPageData.java
@@ -0,0 +1,96 @@
+package ie.sortons.events.shared;
+
+
+import ie.sortons.events.client.view.overlay.ClientPageDataOverlay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
+public class ClientPageData {
+
+	// this will be either a page id or an app id or an interest list??
+	@Id @Index
+	private String clientPageId;
+
+	private FbPage clientPage;
+
+	private List<FbPage> includedPages = new ArrayList<FbPage>();
+
+	private List<FbPage> ignoredPages = new ArrayList<FbPage>();
+
+	public ClientPageData() { }
+
+	public ClientPageData(ClientPageDataOverlay overlay) {
+		this.clientPageId = overlay.getClientPageId();
+		this.clientPage = new FbPage(overlay.getClientPage());
+
+		for (int i = 0; i < overlay.getIncludedPages().length(); ++i) {
+			includedPages.add(new FbPage(overlay.getIncludedPages().get(i)));
+		}
+
+		for (int i = 0; i < overlay.getIgnoredPages().length(); ++i) {
+			ignoredPages.add(new FbPage(overlay.getIgnoredPages().get(i)));
+		}
+
+	}
+
+	public ClientPageData(FbPage clientPageDetails) {
+		this.clientPage = clientPageDetails;
+		this.clientPageId = clientPageDetails.getPageId();
+		addPage(clientPageDetails);
+	}
+
+	public String getClientPageId() {
+		return this.clientPageId;
+	}
+
+	public FbPage getClientPage(){
+		return this.clientPage;
+	}
+
+	public List<FbPage> getIncludedPages() {
+		return new ArrayList<FbPage>(includedPages);
+	}
+
+	public List<FbPage> getIgnoredPages() {
+		return new ArrayList<FbPage>(ignoredPages);
+	}
+
+
+	public boolean addPage(FbPage page) {
+		boolean added = false;
+
+
+		if(!includedPages.contains(page)){
+			includedPages.add(page);
+			added = true;
+		}
+		if(ignoredPages.contains(page)){
+			ignoredPages.remove(page);
+		}
+		return added;
+	}
+
+	// TODO
+	public boolean ignorePage(FbPage page) {
+
+		boolean excluded = false;
+		if(includedPages.contains(page)){
+			includedPages.remove(page.getPageId());
+			excluded = true;
+		}
+		if(!ignoredPages.contains(page)){
+			ignoredPages.add(page);
+			excluded = true;
+		}else{
+			excluded = true;
+		}
+		return excluded;
+	}
+
+}
---
+++ src/ie/sortons/events/shared/FbPage.java
@@ -0,0 +1,108 @@
+package ie.sortons.events.shared;
+
+import ie.sortons.events.client.view.overlay.FbPageOverlay;
+
+import org.jsonmaker.gwt.client.JsonizerException;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.googlecode.objectify.annotation.Embed;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+
+
+@Embed
+public class FbPage implements FbPageJsonizer, Comparable<FbPage> {
+
+
+	@Id @Index
+	private String pageId;
+	private String name;
+	private String pageUrl;
+
+	public String getPageId(){
+		return pageId;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getPageUrl(){
+		return pageUrl;
+	}
+
+	public FbPage(){
+	}
+
+	public FbPage(String name, String pageUrl, String pageId){
+		this.pageId = pageId;
+		this.name = name;
+		this.pageUrl = pageUrl;
+	}
+
+
+	public FbPage(FbPageOverlay overlay) {
+		this.pageId = overlay.getPageId();
+		this.name = overlay.getName();
+		this.pageUrl = overlay.getPageUrl();
+	}
+
+	public void setPageId(String pageId) {
+		this.pageId = pageId;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+
+	// I think I can leave this alone
+	@Override
+	public Object asJavaObject(JavaScriptObject jsValue)
+			throws JsonizerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// I think I can leave this alone
+	@Override
+	public String asString(Object javaValue) throws JsonizerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// How ot use this on client side and GSON server side?!
+	public String asJsonString() {
+		// Create a FbPageJsonizer instance
+		FbPageJsonizer pj = (FbPageJsonizer)GWT.create(FbPageJsonizer.class);
+		// Jsonize
+		return  pj.asString(this);
+	}
+
+	@Override
+	public int compareTo(FbPage other) {
+		return this.pageId.compareTo(other.getPageId());
+	}
+
+	@Override
+	public final boolean equals(Object obj) {
+	  if (obj == this)
+	    return true;
+	  if (!(obj instanceof FbPage))
+	    return false;
+	  return compareTo((FbPage) obj) == 0;
+	}
+	
+	@Override
+	public final int hashCode() {
+	  return pageId.hashCode();
+	}
+	
+}
