09a8174592bd7865fead391bf485f9dd987a4439
Brian Henry <brian.henry2@gmail.com>
2015-08-12 14:03:43 +0100

    A rename in WallPost -> now changed in endpoint

---
+++ src/test/java/ie/sortons/events/server/servlet/endpoint/RecentPostsEndpointTest.java
@@ -0,0 +1,9 @@
+package ie.sortons.events.server.servlet.endpoint;
+
+public class RecentPostsEndpointTest {
+
+	// Unknown error after uploaded a new version.
+	// 197528567092983 with 4 wallposts in datastore.
+	
+	
+}
